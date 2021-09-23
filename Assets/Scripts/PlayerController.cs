895f0933433639dc8fc8002772cb961c1251d34e
unknown <ffeitosa.leonardo@gmail.com>
2021-09-23 19:02:25 -0300

    add Lerp / Clamp

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,21 +6,50 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float horizontalSpeed = 0.03f;
     [SerializeField] private float forwardSpeed = 0.06f;
+    [SerializeField] private float laneDistanceX = 3;
+
+    Vector3 initialPosition;
+    float targetPositionX;
+
+    private float LeftLaneX => initialPosition.x - laneDistanceX;
+    private float RightLaneX => initialPosition.x + laneDistanceX;
+
+    void Awake()
+    {
+        initialPosition = transform.position;
+    }
 
     void Update()
     {
-        Vector3 targetPosition = transform.position;
-        if(Input.GetKey(KeyCode.A))
-        {   
-            targetPosition += Vector3.left * horizontalSpeed;
+        ProcessInput();
+
+        Vector3 position = transform.position;
+        position.x = ProcessLaneMovement();
+        position.z = ProcessForwardMovement();
+
+        transform.position = position;
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            targetPositionX -= laneDistanceX;
         }
-        
-        if(Input.GetKey(KeyCode.D))
-        {   
-            targetPosition += Vector3.right * horizontalSpeed;
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            targetPositionX += laneDistanceX;
         }
-        
-        targetPosition += Vector3.forward * forwardSpeed;
-        transform.position = targetPosition;
+        targetPositionX = Mathf.Clamp(targetPositionX, LeftLaneX, RightLaneX);
+    }
+
+    private float ProcessLaneMovement()
+    {
+        return Mathf.Lerp(transform.position.x, targetPositionX, Time.deltaTime * horizontalSpeed);
+    }
+
+    private float ProcessForwardMovement()
+    {
+        return transform.position.z + forwardSpeed * Time.deltaTime;
     }
 }
